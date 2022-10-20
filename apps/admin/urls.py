7b5649b6615401d81b4efa12570291db76650524
David Domínguez <dedc_011596@hotmail.com>
2022-10-20 11:19:01 -0500

    Se agrega login y logout al admin

--- apps/admin/urls.py
+++ apps/admin/urls.py
@@ -1,11 +1,13 @@
 # Django
 from django.urls import path
-from apps.admin.views import CreateArtist, ListArtists, Admin
+from apps.admin.views import CreateArtist, ListArtists, Admin, LoginUser, LogoutUser
 
 app_name = 'admin'
 
 urlpatterns = [
     path('', Admin.as_view(), name='home'),
+    path('login', LoginUser.as_view(), name='login'),
+    path('logout', LogoutUser.as_view(), name='logout'),
     path('artistas/listar', ListArtists.as_view(), name='list_artist'),
     path('artistas/crear', CreateArtist.as_view(), name='create_artist')
 ]
--- apps/admin/views.py
+++ apps/admin/views.py
@@ -1,21 +1,33 @@
 # Django
 from django.views.generic import TemplateView, CreateView, ListView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from apps.main.models import Artistas
 from apps.main.forms import ArtistForm
 
 
-class Admin(TemplateView):
+class Admin(LoginRequiredMixin, TemplateView):
+    login_url = reverse_lazy('admin:login')
     template_name = 'admin/home.html'
 
-class CreateArtist(SuccessMessageMixin, CreateView):
+class LoginUser(LoginView):
+    template_name = 'admin/login.html'
+    next_page = reverse_lazy('admin:home')
+
+class LogoutUser(LogoutView):
+    next_page = reverse_lazy('admin:login')
+
+class CreateArtist(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Artistas
     form_class = ArtistForm
     template_name = 'admin/bandas/registro.html'
     success_url = reverse_lazy('admin:create_artist')
     success_message = 'Artista registrado existosamente'
+    login_url = reverse_lazy('admin:login')
 
-class ListArtists(ListView):
+class ListArtists(LoginRequiredMixin, ListView):
     model = Artistas
     template_name = 'admin/bandas/listar.html'
+    login_url = reverse_lazy('admin:login')
