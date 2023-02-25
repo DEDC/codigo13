a95efcf6279dd364a4daa1a549d8a08d9dcc8c7c
David Domínguez <dedc_011596@hotmail.com>
2023-02-25 10:10:35 -0600

    Se comenta el uso de pywhatkit

--- apps/main/views.py
+++ apps/main/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.urls import reverse_lazy
 from django.contrib import messages
 from utils.dates import check_available_date
-import pywhatkit
+# import pywhatkit
 import datetime
 
 class Home(TemplateView):
@@ -41,7 +41,7 @@ class Artista(DetailView):
             f.artista = self.get_object()
             f.save()
             text = '*({})* Hola!, soy *{}* y solicito información para contratar a *{}* para un evento el día *{}* a las *{}* hrs. en *{}*'.format(f.folio, form.cleaned_data['nombre_cto'], f.artista.nombre, form.cleaned_data['fecha'], form.cleaned_data['hora'], form.cleaned_data['lugar'])
-            pywhatkit.sendwhatmsg_instantly("+529931176038", text, 5)
+            # pywhatkit.sendwhatmsg_instantly("+529931176038", text, 5)
             messages.success(self.request, 'Se ha enviado la información para realizar la contratación')
         else:
             messages.error(self.request, 'No se ha podido completar la acción. Inténtelo de nuevo.')
